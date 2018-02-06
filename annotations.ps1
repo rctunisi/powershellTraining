39c904b1793b9f579dcf3645d3fa8d6c1dfb90ec
RCT <pstraining@kasolution.com>
2018-02-06 11:18:25 -0200

    supportShouldProcess e ConfirmImpact

--- annotations.ps1
+++ annotations.ps1
@@ -6,3 +6,18 @@ $cert = get-childitem Certs:\ -recurse –CodeSigningCert
 #assinando scripts
 Set-AuthenticodeSignature -Certificate $cert -FilePath <CAMINHO_DO_SCRIPT>
 
+## supportShouldProcess
+Function Kill-Process{
+    [CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact='Medium')]
+    Param(
+        [String]$ProcessName
+    )
+    PROCESS{
+        $p = Get-Process -Name $ProcessName
+        #funcao para suportar acao do WHATIF e CONFIRM
+        if($PSCmdlet.ShouldProcess($ProcessName,"Terminate process")){
+            $p.kill()
+        }
+    }
+}
+
